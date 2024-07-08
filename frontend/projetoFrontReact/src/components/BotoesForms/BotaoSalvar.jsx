c96347252e1a824d15f19c7f0dd1111d75b006d9
angelicaccampos <angelicadacostacampos@gmail.com>
2024-07-08 20:06:32 -0300

    criar página inicial e components

---
+++ frontend/projetoFrontReact/src/components/BotoesForms/BotaoSalvar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+function BotaoSalvar() {
+  return (
+    <button className={styles.buttonSalvar}>Salvar</button>
+  );
+}
+
+export default BotaoSalvar;
